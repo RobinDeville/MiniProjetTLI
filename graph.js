edf4b53edbb985079ab9c4f3ca489deb38ff9b5d
RobinDeville <47632795+RobinDeville@users.noreply.github.com>
2019-03-20 15:19:17 +0100

    Update graph.js

--- graph.js
+++ graph.js
@@ -9,34 +9,26 @@ document.getElementById("graph").onmousedown  = function(event) {
   event = event || window.event;
   event.preventDefault();
   getPosition(this, event);
-}
+  }
 }
 
-function getPosition(canvas, event) {
-    //var canvas = document.getElementById("graph");
-    
+
+function getPosition(canvas, event) {    
     var abs = event.clientX;
     var ord = event.clientY;
-    
     var x = abs - canvas.offsetLeft;
     var y = ord - canvas.offsetTop;
-    
-    
     console.log(x,y);
-    
-  
-     graph(x,y);
+    graph(x,y);
 }
 
 
-
-
-
- function graph(x,y) {
+function graph(x,y) {
    var id = document.getElementById("graph");
    var context = id.getContext("2d");
    context.fillStyle = "#FF0000";
    context.beginPath();
    context.arc(x, y, 7, 0, Math.PI * 2, true);
    context.fill();
+   context.stroke();
 }
