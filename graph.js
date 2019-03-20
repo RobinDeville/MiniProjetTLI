f730722ac3fd3e9beea7414e2183ba676c15d418
RobinDeville <47632795+RobinDeville@users.noreply.github.com>
2019-03-20 15:00:48 +0100

    Update graph.js

--- graph.js
+++ graph.js
@@ -2,11 +2,41 @@
 // R.Devillebichot & M.Bohard
 // Mini-projet TLI
 
-function maison() {
-  var id = document.getElementById("maison");
-  var context = id.getContext("2d");
+
+
+function Click() {
+document.getElementById("graph").onmousedown  = function(event) {
+  event = event || window.event;
+  event.preventDefault();
+  getPosition(this, event);
+}
 }
 
-    ctx.beginPath();
-    ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
-    ctx.fill();
+function getPosition(canvas, event) {
+    //var canvas = document.getElementById("graph");
+    
+    var abs = event.clientX;
+    var ord = event.clientY;
+    
+    var x = abs - canvas.offsetLeft;
+    var y = ord - canvas.offsetTop;
+    
+    
+    console.log(x,y);
+    
+  
+     graph(x,y);
+}
+
+
+
+
+
+ function graph(x,y) {
+   var id = document.getElementById("graph");
+   var context = id.getContext("2d");
+   context.fillStyle = "#FF0000";
+   context.beginPath();
+   context.arc(x, y, 7, 0, Math.PI * 2, true);
+   context.fill();
+}
