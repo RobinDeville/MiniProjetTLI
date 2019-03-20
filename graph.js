22d828f295f1acdb35430facf0a02f3a64e9e05f
maxencebohard <47632799+maxencebohard@users.noreply.github.com>
2019-03-20 14:34:24 +0100

    Update graph.js

--- graph.js
+++ graph.js
@@ -6,3 +6,7 @@ function maison() {
   var id = document.getElementById("maison");
   var context = id.getContext("2d");
 }
+
+    ctx.beginPath();
+    ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
+    ctx.fill();
