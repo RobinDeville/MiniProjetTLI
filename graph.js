4123373400a6619323ed14682701a091d77a4258
RobinDeville <47632795+RobinDeville@users.noreply.github.com>
2019-04-02 16:56:14 +0200

    Update graph.js

--- graph.js
+++ graph.js
@@ -2,12 +2,14 @@
 // R.Devillebichot & M.Bohard
 // Mini-projet TLI
 
-var tableauPoints = new Array();
+//var tableauPoints = new Array();
 
 var posX = new Array();
 var posY = new Array();
 var id = document.getElementById("graph");
 var context = id.getContext("2d");
+var TabLignesDebut = new Array();
+var TabLignesFin = new Array();
 var rang = 0;
 var rangLigne = 0;
 var pointSelectione = -1;
@@ -15,7 +17,9 @@ var test = 0;
 var XDebut = -1;
 var YDebut = -1;
 var XFin = -1;
-var YFin = 0;
+var YFin = -1;
+
+
 
 function Click() {
 document.getElementById("graph").onmousedown  = function(event) {
@@ -37,7 +41,7 @@ function getPosition(canvas, event) {
     
     var j = VerifPoint(x,y);
     //console.log("j = "+j);
-    console.log("pointSelectione = "+pointSelectione);
+    //console.log("pointSelectione = "+pointSelectione);
 	
     if (j >= -1){
       console.log("Déjà un point ici");
@@ -89,15 +93,18 @@ function VerifPoint(x,y){
 function CreatePoint(x,y) {
    XDebut = -1;
    YDebut = -1;
+   
    context.fillStyle = "#FF0000";
    context.beginPath();
+   context.strokeStyle =  "#000000";
+   context.lineWidth = 1;
    context.arc(x, y, 6, 0, Math.PI * 2, true);
    context.fill();
    context.stroke();
    
    posX[rang] = x;
    posY[rang] = y;
-   console.log(" posY[rang] "+ posY[rang]);
+   console.log(" posY["+rang+"] "+ posY[rang]);
    rang++;
    
    //console.log(posX);
@@ -105,13 +112,32 @@ function CreatePoint(x,y) {
    
 }
 
+function fnZero(){
+  console.log("Debut");
+  context.clearRect(0,0,400,400);
+  delete(posX);
+  delete(posY);
+  delete(rang);
+  delete(rangLigne);
+  delete(pointSelectione);
+  delete(test);
+  delete(XDebut);
+  delete(YDebut);
+  delete(XFin);
+  delete(YFin);
+  console.log("Fin");
+}
+
 function StockCoord(pointSelectione){
     
-  console.log("XDebut = "+XDebut);	
+  console.log("pointSelectione = "+pointSelectione);	
   
     if ((XDebut == -1) || (YDebut == -1)) {
       console.log("Stockage des coordonnées du point de départ de la ligne");
       console.log("Coordonnées du point sélectionné : x = "+posX[pointSelectione+1]+", y = "+posY[pointSelectione+1]);
+      
+      TabLignesDebut[rangLigne] = pointSelectione+1;
+      
       XDebut = posX[pointSelectione+1];
       YDebut = posY[pointSelectione+1];
     } else {
@@ -119,24 +145,25 @@ function StockCoord(pointSelectione){
       console.log("Coordonnées du point sélectionné : x = "+posX[pointSelectione+1]+", y = "+posY[pointSelectione+1]);
       XFin = posX[pointSelectione+1];
       YFin = posY[pointSelectione+1];
-      
-      
-
       console.log("Traçage en partant du point "+XDebut+","+YDebut+" vers le point "+XFin+","+YFin+"...");
       context.beginPath();
       context.strokeStyle =  "#00b33c";
-      console.log("tracé en cours...");	
-      context.lineWidth = 5;
+      context.lineWidth = 3;
       context.moveTo(XDebut,YDebut);
       context.lineTo(XFin,YFin);
       context.stroke();
       XDebut = -1;
       YDebut = -1;
-      //repaint();
-      
+      TabLignesFin[rangLigne] = pointSelectione+1;
+      rangLigne++;
     }
       
       XFin = -1;
       YFin = -1;
+    context.restore();
     console.log("Après boucle : XDebut = "+XDebut);
+    
+    console.log(TabLignesFin[0]);
 }
+
+
